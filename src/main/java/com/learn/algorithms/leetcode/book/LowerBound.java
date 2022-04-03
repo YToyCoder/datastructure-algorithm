814896d186df7f22e8951cfc0299a38c336812e4
forsilence <3312034934@qq.com>
2022-04-03 09:26:33 +0800

    LowerBound -- Book

---
+++ src/main/java/com/learn/algorithms/leetcode/book/LowerBound.java
@@ -0,0 +1,15 @@
+package com.learn.algorithms.leetcode.book;
+
+import java.util.Objects;
+
+public class LowerBound {
+	public int lowerBound(int[] nums, int target){
+		int len;
+		if(Objects.isNull(nums) || (len = nums.length) == 0 || nums[0] >= target) return -1;
+		int min = 0;
+		for(int i=0; i<len && nums[i] < target; ++i){
+			if(nums[min] <= nums[i]) min = i; 
+		}
+		return min;
+	} 
+}
--- src/test/java/com/learn/algorithms/LeetcodeTest.java
+++ src/test/java/com/learn/algorithms/LeetcodeTest.java
@@ -1,6 +1,7 @@
 package com.learn.algorithms;
 
 import com.learn.algorithms.leetcode.book.CountAndSay;
+import com.learn.algorithms.leetcode.book.LowerBound;
 import com.learn.algorithms.leetcode.book.RemoveElement;
 
 import junit.framework.TestCase;
@@ -29,5 +30,22 @@ public class LeetcodeTest extends TestCase {
 		RemoveElement code = new RemoveElement();
 		int res1 = code.removeElement(new int[]{3,2,2,3}, 3);
 		assertEquals(2, res1);
+	}
+	
+
+	public void testLowerBound(){
+		LowerBound code = new LowerBound();
+		int res1 = code.lowerBound(new int[]{1,1,2,2,3,4}, 3);
+		assertEquals(3, res1);
+		int res2 = code.lowerBound(new int[]{1,1,2,2,3,4}, 0);
+		assertEquals("{1,1,2,2,3,4},0", -1, res2);
+		int res3 = code.lowerBound(new int[]{1,1,2,2,3,4}, -1);
+		assertEquals("{1,1,2,2,3,4},-1",-1, res3);
+		int res4 = code.lowerBound(new int[]{1,1,2,2,3,4}, 1);
+		assertEquals("{1,1,2,2,3,4},1",-1, res4);
+		int res5 = code.lowerBound(new int[]{0,1,2,2,3,4}, 1);
+		assertEquals("{0,1,2,2,3,4},1", 0, res5);
+		int res6 = code.lowerBound(new int[]{-4,-4,2,2,3,4}, -1);
+		assertEquals("{-4,-4,2,2,3,4},0", 1, res6);
 	}
 }
