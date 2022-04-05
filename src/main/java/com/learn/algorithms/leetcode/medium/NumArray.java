cae8aa36eefc9003c239571819d536eaceadf137
forsilence <3312034934@qq.com>
2022-04-05 20:59:39 +0800

    NumArray not finished

---
+++ src/main/java/com/learn/algorithms/leetcode/medium/NumArray.java
@@ -0,0 +1,14 @@
+package com.learn.algorithms.leetcode.medium;
+
+public class NumArray {
+	
+	public NumArray(int[] nums) {
+	}
+	
+	public void update(int index, int val) {
+	}
+	
+	public int sumRange(int left, int right) {
+		return 1;
+	}
+}
