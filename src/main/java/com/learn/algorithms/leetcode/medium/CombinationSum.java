1c4c8dc13b78e8d901fed4a9f22562b81c0ea95d
forsilence <3312034934@qq.com>
2022-04-18 20:25:13 +0800

    --

--- src/main/java/com/learn/algorithms/leetcode/medium/CombinationSum.java
+++ src/main/java/com/learn/algorithms/leetcode/medium/CombinationSum.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 public class CombinationSum {
 
-	// 	给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
+	// 给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target
+	// 的 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
 	// candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。 
 	// 对于给定的输入，保证和为 target 的不同组合数少于 150 个。
 	// 来源：力扣（LeetCode）
@@ -17,24 +18,25 @@ public class CombinationSum {
 		backtrace(store, candidates, temp, target, 0);
 		return store;
 	}
-	
-	private void backtrace(List<List<Integer>> store, int[] candidates, List<Integer> ints, int target, int begin){
-		if(begin >= candidates.length)return ;
+
+	private void backtrace(List<List<Integer>> store, int[] candidates, List<Integer> ints, int target, int begin) {
+		if (begin >= candidates.length)
+			return;
 		int sum = ints.stream().reduce(0, (a, b) -> a + b);
-		if(sum == target){
+		if (sum == target) {
 			store.add(new ArrayList<>(ints));
-		}else if(sum < target){
+		} else if (sum < target) {
 			ints.add(0);
-			for(int i=begin; i<candidates.length; i++){
+			for (int i = begin; i < candidates.length; i++) {
 				ints.set(ints.size() - 1, candidates[i]);
 				backtrace(store, candidates, ints, target, i);
 			}
 			ints.remove(ints.size() - 1);
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		CombinationSum code = new CombinationSum();
-		code.combinationSum(new int[]{2,3,6,7}, 7);
+		code.combinationSum(new int[] { 2, 3, 6, 7 }, 7);
 	}
 }
