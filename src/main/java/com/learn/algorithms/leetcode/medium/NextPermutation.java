a63072559141fa8b0966770222fb2e9b30621189
forsilence <3312034934@qq.com>
2022-04-22 00:07:24 +0800

    leetcode

--- src/main/java/com/learn/algorithms/leetcode/medium/NextPermutation.java
+++ src/main/java/com/learn/algorithms/leetcode/medium/NextPermutation.java
@@ -1,43 +1,51 @@
 package com.learn.algorithms.leetcode.medium;
 
-import java.util.Arrays;
-
 public class NextPermutation {
 
 	public void nextPermutation(int[] nums) {
 		int len = nums.length;
-		if(len > 1){
-			boolean has = false;
-			int i=0;
-			for(; i < len - 1; i++){
-				int min = -1;
-				for(int j=i+1; j < len; j++){
-					if(nums[j] > nums[i] && 
-						((min != -1 && nums[j] < nums[min]) || min == -1)
-						){
-						min = j;
+		boolean flag = false;
+		for (int i = len - 1; i > 0; i--) {
+			if (nums[i] > nums[i - 1]) {
+				flag = true;
+				int front = nums[i - 1];
+				if (i + 1 == len) {
+					nums[i - 1] = nums[i];
+					nums[i] = front;
+				}else{
+					int j;
+					for (j= i + 1; j < len; j++) {
+						if (nums[j] <= front) {
+							break;
+						}
 					}
-					if(nums[j - 1] < nums[j])
-						has = true;
+					nums[i - 1] = nums[j - 1];
+					nums[j - 1] = front;
+					for (int k = j - 1; k < len - 1; k++) {
+						if (nums[k + 1] > nums[k]) {
+							front = nums[k + 1];
+							nums[k + 1] = nums[k];
+							nums[k] = front;
+						}
+					}
+					for (int k = 0; k < (len - i) / 2; k++) {
+						front = nums[i + k];
+						nums[i + k] = nums[len - k - 1];
+						nums[len - k - 1] = front;
+					}
 				}
-				if(!has) break;
-				if(min != -1){
-					int tmp = nums[i];
-					nums[i] = nums[min];
-					nums[min] = tmp;
-					break;
-				}
-			}
-			if(!has){
-				int tmp;
-				for(i=0; i<len/2; i++){
-					tmp = nums[i];
-					nums[i] = nums[len - i - 1];
-					nums[len - i - 1] = tmp;
-				}
-			}else{
-				Arrays.sort(nums, i + 1, len);
+				break;
 			}
 		}
+		if(!flag)
+			for (int i = 0; i < len / 2; i++) {
+				int temp = nums[i];
+				nums[i] = nums[len - i - 1];
+				nums[len - i - 1] = temp;
+			}
+	}
+	public static void main(String[] args) {
+		new NextPermutation().nextPermutation(new int[]{1,5,1});
+		System.out.println("");
 	}
 }
