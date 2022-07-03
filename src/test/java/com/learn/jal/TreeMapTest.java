c5e6660fdcb68a39a4ad677cb558c864c2a03d56
forsilence <3312034934@qq.com>
2022-07-03 09:28:44 +0800

    TreeMapTest:computeIfAbsent

---
+++ src/test/java/com/learn/jal/TreeMapTest.java
@@ -0,0 +1,19 @@
+package com.learn.jal;
+
+import static org.junit.Assert.assertNull;
+
+import java.util.TreeMap;
+
+import org.junit.Test;
+
+public class TreeMapTest {
+
+  @Test
+  public void testComputeIf(){
+    TreeMap<String, String> map = new TreeMap<>();
+    map.put("key", null);
+    var value = map.computeIfAbsent("key", key -> "null");
+    assertNull(value);
+  }
+
+}
