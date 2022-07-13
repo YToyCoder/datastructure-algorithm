f20f6d14be1cae783fea8337ca5aeed571b5c3e2
forsilence <3312034934@qq.com>
2022-07-13 13:32:13 +0800

    search-algorithms #find-k-closest

---
+++ src/main/java/com/learn/algorithms/SearchAndSort.java
@@ -0,0 +1,59 @@
+package com.learn.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+
+public class SearchAndSort {
+
+  public static <T> List<T> findKClosest(
+    List<T> ls, // source
+    int k, // number
+    T x, // close to x
+    /** (a, b) => a - b */
+    BiFunction<T,T, Integer> compare,
+    BiFunction<T,T, Integer> distanceBetween
+  ) {
+    int crossover_point = findCrossOver(ls, 0, ls.size() - 1, x, compare);
+    // left and right
+    /**
+     * <--- left ---- right --->
+     */
+    int left = crossover_point, right = crossover_point + 1; 
+    int count = 0;
+    List<T> ans = new ArrayList<>(k);
+    while(left >= 0 && right < ls.size() && count < k){
+      if(distanceBetween.apply(ls.get(left),x) >= distanceBetween.apply(ls.get(right), x))
+        ans.add(ls.get(left--));
+      else ans.add(ls.get(right++));
+    }
+    return ans;
+  }
+
+  /**
+   * find the el (cross over point) index before which are smaller than or equal to {@code x} and after which greater than {@code x}
+   * binary search
+   * @param <T>
+   * @param arr
+   * @param start
+   * @param end
+   * @param x
+   * @return
+   */
+  private static <T> int findCrossOver(List<T> arr, int start, int end, T x, BiFunction<T,T,Integer> compare){
+    if(compare.apply(x, arr.get(end)) > 0) return end;
+    if(compare.apply(x, arr.get(start)) < 0) return start;
+    int left = start, right = end, mid = (left + right) / 2;
+    while(compare.apply(arr.get(mid), x) > 0 || compare.apply(arr.get(mid + 1), x) <= 0){
+      if(compare.apply(arr.get(mid), x) > 0){
+        right = mid;
+      }else 
+        left = mid;
+    }
+    return mid;
+  }
+
+  public static <T extends Comparable<T> > List<T> findKclosest(List<T> ls, int k, T x, BiFunction<T,T,Integer> distanceBetween){
+    return findKClosest(ls, k, x, T::compareTo, distanceBetween);
+  }
+}
