ddc367e286ba4eb3207961cacd26159347438d59
forsilence <3312034934@qq.com>
2022-07-28 11:38:54 +0800

    leetcode CanBeEqual new solution

--- src/main/java/com/learn/algorithms/leetcode/easy/CanBeEqual.java
+++ src/main/java/com/learn/algorithms/leetcode/easy/CanBeEqual.java
@@ -1,14 +1,20 @@
 package com.learn.algorithms.leetcode.easy;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CanBeEqual {
   
   public boolean canBeEqual(int[] target, int[] arr) {
-    Arrays.sort(target);
-    Arrays.sort(arr);
-    for(int i=0; i<target.length;i++){
-      if(target[i] != arr[i]) return false;
+    Map<Integer, Integer> map = new HashMap<>();
+    for(int i=0; i<target.length; i++){
+      if(target[i] != arr[i]){
+        map.put(target[i], map.getOrDefault(target[i], 0) + 1);
+        map.put(arr[i], map.getOrDefault(arr[i], 0) - 1);
+      }
+    }
+    for(Map.Entry<Integer, Integer> el : map.entrySet()){
+      if(el.getValue() != 0) return false;
     }
     return true;
   }
