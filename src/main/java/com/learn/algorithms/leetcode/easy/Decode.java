6e00174a038217f4ff3da2af1d0def846d4f2210
forsilence <3312034934@qq.com>
2022-07-28 11:13:50 +0800

    leetcode Decode

---
+++ src/main/java/com/learn/algorithms/leetcode/easy/Decode.java
@@ -0,0 +1,13 @@
+package com.learn.algorithms.leetcode.easy;
+
+public class Decode {
+  
+  public int[] decode(int[] encoded, int first) {
+    int[] ans = new int[encoded.length + 1];
+    ans[0] = first;
+    for(int i=1; i<ans.length; i++){
+      ans[i] = ans[i - 1] ^ encoded[i - 1];
+    }
+    return ans;
+  }
+}
