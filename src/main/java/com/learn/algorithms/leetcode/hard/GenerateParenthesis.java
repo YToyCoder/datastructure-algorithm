2193a6490f2aaa09974a7b62c7a6a4db37f4ed6e
forsilence <3312034934@qq.com>
2022-08-01 20:59:55 +0800

    Leetcode GenerateParenthesis short one

--- src/main/java/com/learn/algorithms/leetcode/hard/GenerateParenthesis.java
+++ src/main/java/com/learn/algorithms/leetcode/hard/GenerateParenthesis.java
@@ -13,29 +13,19 @@ public class GenerateParenthesis {
   }
 
   List<String> generate(int n, Map<Integer, List<String>> cache){
-    if(n <= 0) return List.of();
+    if(n <= 0) return List.of("");
     List<String> mayCached = cache.get(n);
     if(Objects.nonNull(mayCached)) 
       return mayCached;
     if(n == 1) return List.of("()");
     List<String> tocache = new LinkedList<>();
-    List<String> subone = generate(n - 1, cache);
-    for(String s : subone){
-      tocache.add("()" + s);
-    }
-    for(int i=1; i < n; i++){
+    for(int i=0; i < n; i++){
       List<String> leftIns = generate(i, cache);
       List<String> rights = generate(n - i - 1, cache);
       for(String leftIn : leftIns){
         for(String right : rights){
           // (leftIn)right
           tocache.add("(" + leftIn + ")" + right);
-        }
-      }
-      if(rights.isEmpty()){
-        for(String leftIn : leftIns){
-            // (leftIn)right
-            tocache.add("(" + leftIn + ")" );
         }
       }
     }
