533df5df45ec409379d92c0db9f981831cda74f2
forsilence <3312034934@qq.com>
2022-08-08 09:03:58 +0800

    Leetcode DeleteNode

--- src/main/java/com/learn/algorithms/leetcode/easy/DeleteNode.java
+++ src/main/java/com/learn/algorithms/leetcode/easy/DeleteNode.java
@@ -1,5 +1,7 @@
 package com.learn.algorithms.leetcode.easy;
 
+import java.util.Objects;
+
 import com.learn.utils.ListNode;
 
 public class DeleteNode {
@@ -9,5 +11,19 @@ public class DeleteNode {
     node.val = todelete.val;
     node.next = todelete.next;
     todelete.next = null;
+  }
+
+
+  public ListNode deleteNode(ListNode head, int val) {
+    while(Objects.nonNull(head) && head.val == val) 
+      head = head.next;
+    ListNode ans = head;
+    while(Objects.nonNull(head) && Objects.nonNull(head.next)){
+      if(head.next.val == val){
+        head.next = head.next.next;
+      }
+      head = head.next;
+    }
+    return ans;
   }
 }
