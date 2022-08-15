38db8e440ed698855b093b8e707eff75f32a841e
forsilence <3312034934@qq.com>
2022-08-15 17:09:20 +0800

    Leetcode
    #medium
    
    numSubarrayProductLessThanK
    
    乘积小于 K 的子数组

--- src/main/java/com/learn/algorithms/leetcode/medium/Medium.java
+++ src/main/java/com/learn/algorithms/leetcode/medium/Medium.java
@@ -210,4 +210,66 @@ public class Medium {
     }
   }
 
+//
+//  给你一个整数数组 nums 和一个整数 k ，请你返回子数组内所有元素的乘积严格小于 k 的连续子数组的数目。
+//       
+//
+//  示例 1：
+//
+//  输入：nums = [10,5,2,6], k = 100
+//  输出：8
+//  解释：8 个乘积小于 100 的子数组分别为：[10]、[5]、[2],、[6]、[10,5]、[5,2]、[2,6]、[5,2,6]。
+//  需要注意的是 [10,5,2] 并不是乘积小于 100 的子数组。
+//  示例 2：
+//
+//  输入：nums = [1,2,3], k = 0
+//  输出：0
+//
+//  来源：力扣（LeetCode）
+//  链接：https://leetcode.cn/problems/subarray-product-less-than-k
+//  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+  public int numSubarrayProductLessThanK(int[] nums, int k) {
+    int multi = 1;
+    int[] left_position = new int[nums.length];
+    int left = find_smaller_el(nums, left_position, 0, k);
+    for(int i=left; i<nums.length; i++){
+      if(nums[i] * multi < k){
+        left_position[i] = left;
+        multi *= nums[i];
+      }else { // >= k
+        multi *= nums[i] ;
+        while(left < i && multi >= k){
+          multi /= nums[left++];
+        }
+        if(left == i){
+          multi = 1;
+          i = left = find_smaller_el(nums, left_position, i, k);
+          if(left < nums.length){
+            left_position[i] = i;
+            multi = nums[left];
+          }
+        } else left_position[i] = left;
+      }
+    }
+    // reverse
+    int count = 0;
+    for(int i = nums.length - 1; i >= 0 ; i--){
+      count += switch (left_position[i]){
+        case -1 -> 0;
+        default -> {
+          if(left_position[i] == i) yield 1;
+          yield i - left_position[i] + 1;
+        }
+      };
+    }
+    return count;
+  }
+
+  private int find_smaller_el(int[] nums, int[] left_position, int start, int k){
+    while(start < nums.length && nums[start] >= k)
+      left_position[start++] = -1;
+    return start;
+  }
+
+
 }
--- src/test/java/com/learn/algorithms/LeetcodeMedium.java
+++ src/test/java/com/learn/algorithms/LeetcodeMedium.java
@@ -1,6 +1,7 @@
 package com.learn.algorithms;
 
 import com.learn.algorithms.leetcode.medium.Medium;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class LeetcodeMedium {
@@ -9,5 +10,13 @@ public class LeetcodeMedium {
   @Test
   public void solveEquation(){
     code.solveEquation("x+5-3+x=6+x-2");
+  }
+
+  @Test
+  public void numSubarrayProductLessThanK_test(){
+    Assert.assertEquals(8, code.numSubarrayProductLessThanK(new int[]{10, 5, 2, 6}, 100));
+    Assert.assertEquals(0, code.numSubarrayProductLessThanK(new int[]{10, 5, 2, 6}, 0));
+    Assert.assertEquals(18, code.numSubarrayProductLessThanK(new int[] {10,9,10,4,3,8,3,3,6,2,10,10,9,3}, 19));
+    Assert.assertEquals(11, code.numSubarrayProductLessThanK(new int[] {100, 2, 3, 4, 100, 5, 6, 7, 100}, 100));
   }
 }
