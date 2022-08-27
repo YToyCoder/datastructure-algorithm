ea18239e0b99b7a24a64c515510870d6bca73165
forsilence <3312034934@qq.com>
2022-08-27 17:40:34 +0800

    leetcode widthOfBinaryTree 二叉树层序遍历

--- src/main/java/com/learn/algorithms/leetcode/medium/Medium.java
+++ src/main/java/com/learn/algorithms/leetcode/medium/Medium.java
@@ -1,12 +1,16 @@
 package com.learn.algorithms.leetcode.medium;
 
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.TreeMap;
+
+import com.learn.utils.TreeNode;
 
 public class Medium {
 
@@ -307,5 +311,52 @@ public class Medium {
     return medium;
   }
 
+
+  //  662. 二叉树最大宽度
+  // 给你一棵二叉树的根节点 root ，返回树的 最大宽度 。
+
+  // 树的 最大宽度 是所有层中最大的 宽度 。
+
+  // 每一层的 宽度 被定义为该层最左和最右的非空节点（即，两个端点）之间的长度。将这个二叉树视作与满二叉树结构相同，两端点间会出现一些延伸到这一层的 null 节点，这些 null 节点也计入长度。
+
+  // 题目数据保证答案将会在  32 位 带符号整数范围内。
+
+  //  
+
+  // 来源：力扣（LeetCode）
+  // 链接：https://leetcode.cn/problems/maximum-width-of-binary-tree
+  // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+  // 层序遍历
+  public int widthOfBinaryTree(TreeNode root) {
+    if(Objects.isNull(root)) 
+      return 0;
+    Deque<WithIndex> queue = new LinkedList<>();
+    queue.add(new WithIndex(root, 0));
+    int max_width = 1;
+    while(!queue.isEmpty()){
+      max_width = Math.max(queue.peekLast().index - queue.peekFirst().index + 1 , max_width);
+      final int need2rm = queue.size();
+      for(int i=0; i<need2rm; i++){
+        WithIndex curNode = queue.poll();
+        if( Objects.nonNull( curNode.node.left) ){
+          queue.add(new WithIndex(curNode.node.left, curNode.index * 2));
+        }
+        if( Objects.nonNull( curNode.node.right) ){
+          queue.add(new WithIndex(curNode.node.right, curNode.index * 2 + 1));
+        }
+      }
+    }
+    return max_width;
+  }
+
+  private static class WithIndex {
+    final TreeNode node;
+    final int index;
+
+    public WithIndex(TreeNode _node, int _index){
+      node = _node;
+      index = _index;
+    }
+  }
 
 }
