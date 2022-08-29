6a01debb1560394c0caf7678aa2ebfda8e4a4f5e
forsilence <3312034934@qq.com>
2022-08-29 09:02:05 +0800

    leetcode shuffle

--- src/main/java/com/learn/algorithms/leetcode/easy/Easys.java
+++ src/main/java/com/learn/algorithms/leetcode/easy/Easys.java
@@ -108,4 +108,13 @@ public class Easys {
     return true;
   }
 
+
+  public int[] shuffle(int[] nums, int n) {
+    int[] ans = new int[2 * n];
+    for(int i=0; i<n; i++){
+      ans[i * 2] = nums[i];
+      ans[i * 2+ 1] = nums[n + i]; 
+    }
+    return ans;
+  }
 }
