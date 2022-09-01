5b478ed6d6cabc7c40e2782c1db1e1b9d9fc6e80
forsilence <3312034934@qq.com>
2022-09-01 09:52:38 +0800

    leetcode finalPrices

--- src/main/java/com/learn/algorithms/leetcode/easy/Easys.java
+++ src/main/java/com/learn/algorithms/leetcode/easy/Easys.java
@@ -6,6 +6,7 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Easys {
   
@@ -116,5 +117,33 @@ public class Easys {
       ans[i * 2+ 1] = nums[n + i]; 
     }
     return ans;
+  }
+
+  // 1475. 商品折扣后的最终价格
+  // 给你一个数组 prices ，其中 prices[i] 是商店里第 i 件商品的价格。
+  // 商店里正在进行促销活动，如果你要买第 i 件商品，那么你可以得到与 prices[j] 相等的折扣，其中 j 是满足 j > i 且 prices[j] <= prices[i] 的 最小下标 ，如果没有满足条件的 j ，你将没有任何折扣。
+  // 请你返回一个数组，数组中第 i 个元素是折扣后你购买商品 i 最终需要支付的价格。
+  // 来源：力扣（LeetCode）
+  // 链接：https://leetcode.cn/problems/final-prices-with-a-special-discount-in-a-shop
+  // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+  public int[] finalPrices(int[] prices) {
+    final int len = prices.length;
+    final int[] index4next_less = new int[prices.length];
+    index4next_less[len - 1] = len - 1;
+    for(int i = prices.length - 1; i > 0; i--){
+      int next = i;
+      final int cur = prices[i - 1];
+      while(
+        index4next_less[next] != next && 
+        prices[next] > cur
+      ){
+        next = index4next_less[next];
+      }
+      index4next_less[i - 1] = prices[next] <= cur ? next : i - 1;
+    }
+    for(int i=0; i<len; i++){
+      index4next_less[i] = index4next_less[i] == i ? prices[i] : prices[i] - prices[index4next_less[i]];
+    }
+    return index4next_less;
   }
 }
